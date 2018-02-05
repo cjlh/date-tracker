42de32750197fa163cb388408f3dffbec326e834
Caleb Hamilton <cjlhamilton@gmail.com>
2018-02-05 19:39:49 +0000

    Initial commit - script and README

---
+++ README.md
@@ -0,0 +1,36 @@
+# date-tracker
+
+[Description to be written]
+
+### Prerequisites
+
+This script requires Python 3.
+
+### Installation
+
+Download *dates.py* from this repository to a location of your choice (this is where you will run the script from):
+
+```
+$ wget https://raw.githubusercontent.com/cjlh/date-tracker/master/dates.py
+```
+
+*(Optional)* Set an alias in your shell for quick use. Examples:
+
+For bash (~/.bashrc) and zsh (~/.zshrc):
+```
+alias dates="python3 /path/to/dates.py"
+```
+
+For fish (~/.config/fish/config.fish):
+```
+alias dates "python3 /path/to/dates.py"
+```
+
+
+## Use
+
+[Instructions to be written]
+
+## License
+
+This project is licensed under the GNU GPLv3 License.
---
+++ dates.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""assignments.py
+
+A script to quickly check deadlines for my assignments.
+"""
+
+import datetime
+
+
+terminal_colors = {
+    'blue': '\033[34m',
+    'normal': '\033[0m',
+    'grey': '\033[37m',
+    'white': '\033[97m',
+    'green': '\033[32m',
+    'cyan': '\033[36m',
+    'yellow': '\033[33m',
+    'light_yellow': '\033[93m',
+    'light_blue': '\033[93m',
+    'light_red': '\033[91m'
+}
+
+
+def time_until(y, m, d):
+    return str((datetime.date(y, m, d) - datetime.date.today()).days)
+
+
+def day_of_date(y, m, d):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday"]
+    return days[datetime.datetime(y, m, d).weekday()]
+
+
+def print_assignment_details(title, date, color):
+    print(terminal_colors[color] + title + ': ' +
+          terminal_colors['white'] + time_until(date[0], date[1], date[2]) +
+          terminal_colors['normal'] + ' days (' +
+          day_of_date(date[0], date[1], date[2]) + ')')
+
+
+assignments = [
+    ('Digital Communication', (2018, 2, 16), 'blue'),
+    ('Software Engineering', (2018, 2, 23), 'yellow'),
+    ('Group Project (LSEPI)', (2018, 3, 2), 'yellow'),
+    ('Networks and Systems', (2018, 3, 9), 'normal'),
+    ('Computer Graphics', (2018, 3, 16), 'blue'),
+    ('Theory of Computation benchtest', (2018, 3, 12), 'light_red'),
+    ('Systems Programming bechtest', (2018, 3, 13), 'green')
+]
+
+for assignment in assignments:
+    print_assignment_details(assignment[0], assignment[1], assignment[2])
