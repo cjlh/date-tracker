9f25c37511a3e550d7dc2b507b679ef832f9e557
Caleb Hamilton <cjlhamilton@gmail.com>
2018-02-08 19:25:13 +0000

    Added support for YAML events data file

--- README.md
+++ README.md
@@ -4,19 +4,23 @@ A Python script for keeping track of important dates.
 
 [Further description to be written]
 
-### Prerequisites
+
+## Getting started
+
+
+### Dependencies
 
 This script requires Python 3.
 
+
 ### Installation
 
-Download *dates.py* from this repository to a location of your choice (this is where you will run the script from):
-
+1. Save dates.py to a location of your choice (this is where you will run the script from):
 ```
 $ wget https://raw.githubusercontent.com/cjlh/date-tracker/master/dates.py
 ```
 
-*(Optional)* Set an alias in your shell for quick use. Examples:
+2. *(Optional)* Set an alias in your shell for quick use. Examples:
 
 bash (\~/.bashrc) and zsh (\~/.zshrc):
 ```
@@ -33,6 +37,21 @@ alias dates "python3 /path/to/dates.py"
 
 [Instructions to be written]
 
+
+## TODO
+
+- [ ] Write usage instructions
+- [x] Add support for YAML events file
+- [ ] Rename example 'events.yaml' to 'example_events.yaml' and add support for multiple events files
+- [ ] Enable adding events via the program
+- [ ] Write help text
+- [ ] Comprehensively comment code
+- [ ] Add option to sort events chronologically
+
+
 ## License
 
 This project is licensed under the GNU GPLv3 License.
+
+***
+[https://github.com/cjlh/date-tracker](https://github.com/cjlh/date-tracker)
--- dates.py
+++ dates.py
@@ -4,14 +4,17 @@
 
 File name: dates.py
 Author: Caleb Hamilton
+Website: https://github.com/cjlh/date-tracker
 License: GNU GPLv3
-Python Version: 3
+Python version: 3
 
 Usage:
     $ python dates.py
 """
 
 import datetime
+import yaml
+import sys
 
 
 terminal_colors = {
@@ -28,31 +31,99 @@ terminal_colors = {
 }
 
 
-def time_until(y, m, d):
-    return str((datetime.date(y, m, d) - datetime.date.today()).days)
+def load_events(filepath):
+    """ Loads dates from the given yaml filepath.
+
+    Args:
+        filepath (str): The file from which to load the configuration.
+
+    TODO:
+        - check given date is valid
+        - check given color is valid
+        - add check for '.yml'
+    """
+    with open(filepath, 'r') as events_file:
+        try:
+            events_dict = yaml.safe_load(events_file)
+        except yaml.YAMLError as yaml_e:
+            raise ValueError('Error in \'events.yaml\' file')
+    events_list = []
+    for event in events_dict['events']:
+        events_list.append((event['name'], event['date'], event['color']))
+    return events_list
 
 
-def day_of_date(y, m, d):
+def time_until(date_string):
+    """ Returns the number of days until a given date.
+
+    Args:
+        date_str (str): A date given in ISO 8601 format.
+    """
+    try:
+        # Parse date date object from given string
+        given_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+    except ValueError:
+        print('Error: given date is not in the required format')
+        exit()
+    date_today = datetime.date.today()
+    return str((given_date - date_today).days)
+
+
+def day_of_date(date_string):
+    """ Returns the day of the week of a given date.
+
+    Args:
+        date_string (str): A date given in ISO 8601 format.
+    """
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
             'Friday', 'Saturday', 'Sunday']
-    return days[datetime.datetime(y, m, d).weekday()]
+    try:
+        given_date = datetime.datetime.strptime(date_string, '%Y-%m-%d').date()
+    except ValueError:
+        print('Error: given date is not in the required format')
+        exit()
+    return days[given_date.weekday()]
 
 
-def print_event_details(title, date, color):
-    print(terminal_colors[color] + title + ': ' +
-          terminal_colors['white'] + time_until(date[0], date[1], date[2]) +
+def print_event_details(name, date_string, color):
+    """ Prints out the details of a given event.
+
+    Args:
+        name (str): The name of the event.
+        date_string (str): The date of the event.
+        color (str): The color with which to print out the event's name (this
+            must be a valid key in the `terminal_colors` dictionary.
+    """
+    print(terminal_colors[color] + name + ': ' +
+          terminal_colors['white'] + time_until(date_string) +
           terminal_colors['normal'] + ' days (' +
-          day_of_date(date[0], date[1], date[2]) + ')')
+          day_of_date(date_string) + ')')
+
+
+def help():
+    """ Prints out instructions on how to use this script.
+    """
+    print('TODO')
 
 
 def main():
-    events = [
-        ('Example event', (2345, 6, 7), 'blue'),
-    ]
-
+    """ The main program function.
+    """
+    try:
+        events = load_events('events.yaml')
+    except OSError:
+        print('Error: config file does not exist. For instructions on how to use dates.py, please use the \'-h\' flag (\'python dates.py -h\')')
+        exit()
+    except ValueError:
+        print('Error: invalid config file format. For instructions on how to use dates.py, please use the \'h\' flag (\'python dates.py -h\')')
+        exit()
+    # Loop through events and print the details of each
     for event in events:
         print_event_details(event[0], event[1], event[2])
 
 
 if __name__ == '__main__':
-    main()
+    if (len(sys.argv) == 2 and sys.argv[1] == '-h'):
+        help()
+    else:
+        main()
